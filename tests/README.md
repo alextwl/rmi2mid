45c9b855414d9f0db1b03e033094a7a27457377b
Wei-Li Tang <alex@ip6.tw>
2019-07-10 01:05:25 +0800

    Add tests.

---
+++ tests/README.md
@@ -0,0 +1,7 @@
+# Sample files
+
+* [fur\_Elise\_WoO59.mid][1]: Für Elise by L. V. Beethoven. Retrieved from [the Mutopia Project][2]. Public Domain.
+* fur\_Elise.rmi: RMID version of fur\_Elise\_WoO59.mid.
+
+[1]: https://www.mutopiaproject.org/cgibin/piece-info.cgi?id=931
+[2]: https://www.mutopiaproject.org
---
+++ tests/test_rmi2mid.py
@@ -0,0 +1,29 @@
+import filecmp
+import tempfile
+import os
+import sys
+import unittest
+
+unittestDir = os.path.dirname(os.path.realpath(__file__))
+scriptDir = os.path.abspath(unittestDir + "/..")
+sys.path += [scriptDir]
+
+import rmi2mid
+
+SAMPLE_RMI = unittestDir + "/fur_Elise.rmi"
+SAMPLE_MID = unittestDir + "/fur_Elise_WoO59.mid"
+
+
+class TestRmi2mid(unittest.TestCase):
+    def test_rmi2mid(self):
+        (outhdr, outfile) = tempfile.mkstemp(suffix=".mid")
+        os.close(outhdr)
+        try:
+            rmi2mid.rmi2mid(SAMPLE_RMI, outfile)
+            self.assertTrue(filecmp.cmp(SAMPLE_MID, outfile))
+        finally:
+            os.remove(outfile)
+
+
+if __name__ == '__main__':
+    unittest.main()
